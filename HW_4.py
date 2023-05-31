de35ab393d23ae56c22eb6d9bb2d8c409ae3c193
Meiram <133873249+Salam78@users.noreply.github.com>
2023-05-31 18:19:56 +0600

    Add files via upload

---
+++ HW_4.py
@@ -0,0 +1,9 @@
+a = int(input("Введите число a: "))
+b = int(input("Введите число b: "))
+
+if a > b:
+    result = True
+else:
+    result = False
+print(result)
+input("press any ket to close window")
